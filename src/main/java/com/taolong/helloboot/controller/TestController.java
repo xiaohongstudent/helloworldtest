780549a6532043de0a9362b368a5b2841c9ffe51
taolong.hong <taolong.hong@ubtrobot.com>
2019-05-15 11:02:43 +0800

    test

--- src/main/java/com/taolong/helloboot/controller/TestController.java
+++ src/main/java/com/taolong/helloboot/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.taolong.helloboot.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +9,10 @@ public class TestController {
     @RequestMapping("hello")
     public String hello(){
         return "hello world";
+    }
+
+    @RequestMapping("helloname")
+    public String helloName(@RequestParam  String name){
+        return "hello "+name;
     }
 }
