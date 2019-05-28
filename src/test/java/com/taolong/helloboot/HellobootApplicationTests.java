f5ee8a19db0ae82e810178ea7b6f7cec6d737ee7
taolong.hong <taolong.hong@ubtrobot.com>
2019-05-28 17:20:29 +0800

    test

--- src/test/java/com/taolong/helloboot/HellobootApplicationTests.java
+++ src/test/java/com/taolong/helloboot/HellobootApplicationTests.java
@@ -1,14 +1,19 @@
 package com.taolong.helloboot;
 
+import com.taolong.helloboot.controller.TestController;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class HellobootApplicationTests {
+
+    @Autowired
+    private TestController testController;
 
     @Test
     public void contextLoads() {
@@ -18,6 +23,13 @@ public class HellobootApplicationTests {
     @Test
     public void test(){
         Assert.assertEquals("2","2");
+    }
+
+    @Test
+    public void testControllerHello(){
+        String name = "hongtaolong";
+        String result = testController.helloName(name);
+        Assert.assertEquals("你好啊"+name,result);
     }
     
     /*@Test
